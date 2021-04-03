b9325067112ef4e4bca0fa6399ee3e17a4e6ef30
bbpsystems <78722488+bbpsystems@users.noreply.github.com>
2021-04-03 13:51:52 +0500

    pull_one

---
+++ src/circle_product.js
@@ -0,0 +1,123 @@
+import React from 'react'
+
+export default function circle_product() {
+    return (
+      <div>
+        <section class="w3l-grids-hny-2">
+          <div class="grids-hny-2-mian py-5">
+            <div class="container py-lg-5">
+              <h3 class="hny-title mb-0 text-center">
+                Shop With <span>Us</span>
+              </h3>
+              <p class="mb-4 text-center">Handpicked Favourites just for you</p>
+              <div class="welcome-grids row mt-5">
+                <div class="col-lg-2 col-md-4 col-6 welcome-image">
+                  <div class="boxhny13">
+                    <a href="#URL">
+                      <img
+                        src="assets/images/grid1.jpg"
+                        class="img-fluid"
+                        alt=""
+                      />
+                      <div class="boxhny-content">
+                        <h3 class="title">Product</h3>
+                      </div>
+                    </a>
+                  </div>
+                  <h4>
+                    <a href="#URL">Hand Bags</a>
+                  </h4>
+                </div>
+
+                <div class="col-lg-2 col-md-4 col-6 welcome-image">
+                  <div class="boxhny13">
+                    <a href="#URL">
+                      <img
+                        src="assets/images/grid3.jpg"
+                        class="img-fluid"
+                        alt=""
+                      />
+                      <div class="boxhny-content">
+                        <h3 class="title">Product</h3>
+                      </div>
+                    </a>
+                  </div>
+                  <h4>
+                    <a href="#URL">Watches</a>
+                  </h4>
+                </div>
+                <div class="col-lg-2 col-md-4 col-6 welcome-image">
+                  <div class="boxhny13">
+                    <a href="#URL">
+                      <img
+                        src="assets/images/grid2.jpg"
+                        class="img-fluid"
+                        alt=""
+                      />
+                      <div class="boxhny-content">
+                        <h3 class="title">Product</h3>
+                      </div>
+                    </a>
+                  </div>
+                  <h4>
+                    <a href="#URL">Shoes</a>
+                  </h4>
+                </div>
+                <div class="col-lg-2 col-md-4 col-6 welcome-image">
+                  <div class="boxhny13">
+                    <a href="#URL">
+                      <img
+                        src="assets/images/grid4.jpg"
+                        class="img-fluid"
+                        alt=""
+                      />
+                      <div class="boxhny-content">
+                        <h3 class="title">Product</h3>
+                      </div>
+                    </a>
+                  </div>
+                  <h4>
+                    <a href="#URL">Suits</a>
+                  </h4>
+                </div>
+                <div class="col-lg-2 col-md-4 col-6 welcome-image">
+                  <div class="boxhny13">
+                    <a href="#URL">
+                      <img
+                        src="assets/images/grid5.jpg"
+                        class="img-fluid"
+                        alt=""
+                      />
+                      <div class="boxhny-content">
+                        <h3 class="title">Product</h3>
+                      </div>
+                    </a>
+                  </div>
+                  <h4>
+                    <a href="#URL">Accessories</a>
+                  </h4>
+                </div>
+                <div class="col-lg-2 col-md-4 col-6 welcome-image">
+                  <div class="boxhny13">
+                    <a href="#URL">
+                      <img
+                        src="assets/images/grid6.jpg"
+                        class="img-fluid"
+                        alt=""
+                      />
+                      <div class="boxhny-content">
+                        <h3 class="title">Product</h3>
+                      </div>
+                    </a>
+                  </div>
+                  <h4>
+                    <a href="#URL">Fragrances</a>
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+}
